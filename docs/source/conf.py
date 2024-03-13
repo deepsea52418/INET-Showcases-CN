55393fbe0363d3fd653683bdc7eab3cf44426cbb
Deepsae52418 <Deepsea52418@qq.com>
2024-03-13 10:27:01 +0000

    null

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,11 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ["Pic"]
+html_context = {
+    "github_version": "master",
+    "doc_path": "docs",
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
