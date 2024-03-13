76ab7d88959bb731d3dd6ace2a063ef4de0e2254
Deepsae52418 <Deepsea52418@qq.com>
2024-03-13 10:44:42 +0000

    null

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,6 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-html_video_codec = 'h264'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
